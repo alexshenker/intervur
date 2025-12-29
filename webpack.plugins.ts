dd2eee4c820c7862d44dd0b6e8b62612403e9107
alexshenker <alekseyshenker@gmail.com>
2025-12-29 14:04:54 -0500

    added react refresh with webpack plugin and updated setup to ensure its only active in renderer in dev mode

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,20 @@
-import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import type IForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import type { WebpackPluginInstance } from "webpack";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-export const plugins = [
-  new ForkTsCheckerWebpackPlugin({
-    logger: 'webpack-infrastructure',
-  }),
-];
+export const plugins = (target: "main" | "renderer") => {
+    const basePlugins: WebpackPluginInstance[] = [
+        new ForkTsCheckerWebpackPlugin({
+            logger: "webpack-infrastructure",
+        }),
+    ];
+
+    if (target === "renderer" && process.env.NODE_ENV !== "production") {
+        basePlugins.push(new ReactRefreshWebpackPlugin());
+    }
+
+    return basePlugins;
+};
